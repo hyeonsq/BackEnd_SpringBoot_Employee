e34b6699b93599791bd6b2ef73ac6aa4b1f94b00
hyeon <hyon5807@gmail.com>
2026-03-15 23:04:17 +0900

    update

--- src/main/java/com/employee/api/service/DepartmentServiceImpl.java
+++ src/main/java/com/employee/api/service/DepartmentServiceImpl.java
@@ -1,8 +1,12 @@
 package com.employee.api.service;
 
 import com.employee.api.dto.DepartmentDto;
+import com.employee.api.entity.Department;
+import com.employee.api.exception.ResourceNotFoundException;
+import com.employee.api.mapper.DepartmentMapper;
 import com.employee.api.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,14 +20,34 @@ public class DepartmentServiceImpl implements DepartmentService{
 
     @Override
     public DepartmentDto createDepartment(DepartmentDto departmentDto) {
-        return null;
+        //DTO => Entity 변환
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
+        //등록 처리
+        Department savedDepartment = departmentRepository.save(department);
+        //등록된 Entity => DTO 변환
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public DepartmentDto getDepartmentById(Long departmentId) {
-        return null;
+        /*
+         Department department = departmentRepository.findById(departmentId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+        );
+        //Entity => DTO 변환
+        return DepartmentMapper.mapToDepartmentDto(department);
+         */
+        return departmentRepository.findById(departmentId) //Optional<Department>
+                //.map(department -> DepartmentMapper.mapToDepartmentDto(department))
+                .map(DepartmentMapper::mapToDepartmentDto) //Optional<DepartmentDto)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Department is not exists with a given id: " + departmentId,
+                        HttpStatus.NOT_FOUND));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<DepartmentDto> getAllDepartments() {
         return List.of();
