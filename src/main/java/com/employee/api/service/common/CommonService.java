af424092cafcaed8551f0fabbf0eff6baa1c0208
hyeon <hyon5807@gmail.com>
2026-03-16 15:42:22 +0900

    update

--- src/main/java/com/employee/api/service/common/CommonService.java
+++ src/main/java/com/employee/api/service/common/CommonService.java
@@ -9,7 +9,18 @@ public class CommonService {
     //public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier)
     //Supplier의 추상메서드  T get()
     public static Supplier<ResourceNotFoundException> getNotFoundExceptionSupplier(String msg,
-                                                                                    Long entityId) {
+                                                                                   Long entityId) {
         return () -> new ResourceNotFoundException(msg + entityId, HttpStatus.NOT_FOUND);
+    }
+
+    //Variable Argument(가변적 인자 ) ...
+    public static Supplier<ResourceNotFoundException> getNotFoundExceptionSupplier(String msg, Object... args) {
+        return () -> {
+            StringBuilder fullMsg = new StringBuilder(msg);
+            for (Object arg : args) {
+                fullMsg.append(arg);
+            }
+            return new ResourceNotFoundException(fullMsg.toString(), HttpStatus.NOT_FOUND);
+        };
     }
 }
